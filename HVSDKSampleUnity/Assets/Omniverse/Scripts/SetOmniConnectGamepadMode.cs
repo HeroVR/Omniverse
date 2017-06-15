5ed2647eb6be9d6cc82c96ff1c38e2f11300ac7d
CameronSlayterVirtuix <cameron.slayter@virtuix.com>
2017-06-15 18:23:19 -0500

    Added Set Omni Connect Gamepad Mode
    Added a script to set the mode in Omni Connect to gamepad mode to ensure that it is not improperly in keyboard mode.

---
+++ HVSDKSampleUnity/Assets/Omniverse/Scripts/SetOmniConnectGamepadMode.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+
+public class SetOmniConnectGamepadMode : MonoBehaviour {
+
+
+    private string coroutineUrl;
+    private string serverGetResponse;
+    private string serverPostResponse;
+    private string postString;
+    private string json;
+    private byte[] pData;
+
+    private void Start()
+    {
+        OmniConnectMode omniConnectMode = new OmniConnectMode();
+        omniConnectMode.Data = "Gamepad";
+        json = JsonUtility.ToJson(omniConnectMode);
+        coroutineUrl = "http://localhost:8085/gamemode";
+        pData = Encoding.ASCII.GetBytes(json.ToCharArray());
+        StartCoroutine(CoroutineGetRequest(coroutineUrl));
+    }
+
+    private IEnumerator CoroutinePostRequest(string url, byte[] postRequest)
+    {
+        WWW web = new WWW(url, postRequest);
+        while (!web.isDone)
+        {
+            yield return null;
+        }
+
+        if (web.error != null)
+        {
+            serverPostResponse = "Server Error: " + web.error;
+            Debug.Log(serverPostResponse);
+        }
+        else
+        {
+            serverPostResponse = web.text;
+        }
+    }
+
+    private IEnumerator CoroutineGetRequest(string url)
+    {
+        WWW web = new WWW(url);
+        while (!web.isDone)
+        {
+            yield return null;
+        }
+
+        if (web.error != null)
+        {
+            serverGetResponse = "Server Error: " + web.error;
+        }
+        else
+        {
+            serverGetResponse = web.text;
+            Debug.Log(serverGetResponse);
+            CheckGetResponse(serverGetResponse);
+        }
+    }
+
+    void CheckGetResponse(string getResponse)
+    {
+        if (getResponse != "Gamepad")
+        {
+            StartCoroutine(CoroutinePostRequest(coroutineUrl, pData));
+        }
+    }
+
+}
+
+[Serializable]
+public class OmniConnectMode
+{
+    public string Data;
+}
