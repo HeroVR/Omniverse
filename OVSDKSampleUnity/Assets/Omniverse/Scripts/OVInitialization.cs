77102dcf5f4a4613259b19e6c498d3707a131862
CameronSlayterVirtuix <cameron.slayter@virtuix.com>
2017-10-02 17:08:15 -0500

    Minor adjustments to the Unity SDK
    -Prevented Developer Mode from being true in builds to prevent errors
    -Added OVSetCameraScale script to ensure that the Omniverse System menu laser is in the correct position and rotation even if the character is scaled differently.
    -Added the OVSetCameraScale script to the OVSDK_Camera prefab
    -OVInitialization script has been added to the SDK as a template for CPs. Many CPs have asked for this script.
    -Adjusted some of the translations in the OVSDK_String.xml

---
+++ OVSDKSampleUnity/Assets/Omniverse/Scripts/OVInitialization.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OVInitialization : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!OVSDK.HasInitialized())
+        {
+            //Replace the GameID and GameKey below with the values provided to you for this game by Virtuix.
+            //If you have not received a unique GameID or GameKey, please contact Virtuix to register your game.
+            OVSDK.Init(0000, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "omni=1");
+        }
+    }
+}
---
+++ OVSDKSampleUnity/Assets/Omniverse/Scripts/OVSetOVCameraScale.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OVSetOVCameraScale : MonoBehaviour {
+
+    SteamVR_ControllerManager steamVR_ControllerManager;
+    GameObject leftController;
+    GameObject rightController;
+
+    void Start()
+    {
+        steamVR_ControllerManager = FindObjectOfType<SteamVR_ControllerManager>();
+        leftController = steamVR_ControllerManager.left;
+        rightController = steamVR_ControllerManager.right;
+        if (leftController != null)
+        {
+            gameObject.transform.localScale = leftController.transform.lossyScale;
+        }
+        else if (rightController != null)
+        {
+            gameObject.transform.localScale = rightController.transform.lossyScale;
+        }
+    }
+}
