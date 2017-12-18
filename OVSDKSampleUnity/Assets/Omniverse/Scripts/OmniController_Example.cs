794bb03006c8666f51eb8634c3be7bd27a2c25e0
Laura Brothers <llb09b@acu.edu>
2017-12-18 16:03:18 -0600

    New Spawn Rotation Logic
    Adding new Spawn Rotation logic that corrects the forward direction of the player when they spawn.

--- OVSDKSampleUnity/Assets/Omniverse/Scripts/OmniController_Example.cs
+++ OVSDKSampleUnity/Assets/Omniverse/Scripts/OmniController_Example.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.VR;
 using UnityEngine;
 
 /*
@@ -16,6 +17,10 @@ public class OmniController_Example : MonoBehaviour {
 
     protected OmniMovementComponent MovementComponent;
     private CharacterController m_CharacterController;
+    private Camera cameraRef;
+
+    private bool RotationCorrected = false;
+
 
     // Use this for initialization
     void Start ()
@@ -24,12 +29,38 @@ public class OmniController_Example : MonoBehaviour {
         m_CharacterController = GetComponent<CharacterController>();
     }
 
+    void CorrectSpawnForward()
+    {
+        Debug.Log(VRDevice.model + " connected.");
+
+        RotationCorrected = true;
+
+        Vector3 resultRotation = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 spawnRotation = transform.rotation.eulerAngles;
+        cameraRef = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        Vector3 cameraRotation = cameraRef.transform.rotation.eulerAngles;
+
+        float spawnRotationYaw = spawnRotation.y;
+        float cameraYaw = cameraRotation.y;
+        float difference = spawnRotationYaw - cameraYaw;
+
+        resultRotation.y = difference;
+
+        transform.Rotate(resultRotation);
+    }
+
 
     // Update is called once per frame
     void Update ()
     {
+        if(!RotationCorrected)
+        {
+            if(VRDevice.isPresent)
+                CorrectSpawnForward();
+        }
+
         UseOmniInputToMovePlayer();
-	}
+    }
 
 
     // Gets Omni Movement Vectors from the OmniMovementComponent and uses them to Move the Player
@@ -37,7 +68,7 @@ public class OmniController_Example : MonoBehaviour {
     {
         if (MovementComponent.omniFound)
             MovementComponent.GetOmniInputForCharacterMovement();
-        else if (MovementComponent.developerMode)
+       else if (MovementComponent.developerMode)
             MovementComponent.DeveloperModeUpdate();
 
 
